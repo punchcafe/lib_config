cfbee0a38ff87810e1beebb74d820e7729c486e8
Luca Fruzza <lucaf@eu.square-enix.com>
2023-12-23 00:23:37 +0000

    :sparkles: initial LibConfig.validate/1 and macro functionality

--- lib/lib_config.ex
+++ lib/lib_config.ex
@@ -3,16 +3,26 @@ defmodule LibConfig do
   Documentation for `LibConfig`.
   """
 
-  @doc """
-  Hello world.
+  def validate(module) do
+    definition = module.__lib_config_field__(:definition)
+    all_envs = Application.get_all_env(module.__lib_config_field__(:app_name))
 
-  ## Examples
+    with {:ok, _} <- NimbleOptions.validate(all_envs, definition) do
+      :ok
+    end
+  end
 
-      iex> LibConfig.hello()
-      :world
+  defmacro __using__(opts) do
+    definition = opts[:definition] || raise "must provide a definition"
+    app_name = opts[:app_name] || raise "must provide an app name"
 
-  """
-  def hello do
-    :world
+    quote do
+      def __lib_config_field__(:app_name), do: unquote(app_name)
+      def __lib_config_field__(:definition), do: unquote(Macro.escape(definition))
+
+      def validate() do
+        LibConfig.validate(__MODULE__)
+      end
+    end
   end
 end
--- test/lib_config_test.exs
+++ test/lib_config_test.exs
@@ -2,7 +2,56 @@ defmodule LibConfigTest do
   use ExUnit.Case
   doctest LibConfig
 
-  test "greets the world" do
-    assert LibConfig.hello() == :world
+  defmodule LibConfigTestModule do
+    use LibConfig,
+      app_name: :my_test_app,
+      definition: [
+        test_integer: [
+          type: :non_neg_integer,
+          required: true
+        ],
+        test_url: [
+          type: :string,
+          required: true
+        ]
+      ]
+  end
+
+  test "validates a config list when correct" do
+    Application.put_env(:my_test_app, :test_integer, 5)
+    Application.put_env(:my_test_app, :test_url, "http://www.helloworld.com")
+    assert LibConfigTestModule.validate() == :ok
+  end
+
+  test "adds private function __lib_config_field__(:app_name)" do
+    assert :my_test_app == LibConfigTestModule.__lib_config_field__(:app_name)
+  end
+
+  test "adds private function __lib_config_field__(:definition)" do
+    assert [
+             test_integer: [
+               type: :non_neg_integer,
+               required: true
+             ],
+             test_url: [
+               type: :string,
+               required: true
+             ]
+           ] == LibConfigTestModule.__lib_config_field__(:definition)
+  end
+
+  test "raises an exception when invalid application config" do
+    Application.put_env(:my_test_app, :test_integer, "not_an_integer")
+    Application.put_env(:my_test_app, :test_url, "http://www.helloworld.com")
+
+    assert LibConfigTestModule.validate() ==
+             {:error,
+              %NimbleOptions.ValidationError{
+                message:
+                  "invalid value for :test_integer option: expected non negative integer, got: \"not_an_integer\"",
+                key: :test_integer,
+                value: "not_an_integer",
+                keys_path: []
+              }}
   end
 end
