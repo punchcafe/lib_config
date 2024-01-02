2f1c4dcb2f0eaa1116695eee2ba82724ea7a7fed
Luca Fruzza <lucaf@eu.square-enix.com>
2024-01-02 22:18:14 +0000

    :wrench: make dialyxir only dev and test

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule LibConfig.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, ">= 0.0.0"},
+      {:dialyxir, ">= 0.0.0", runtime: false, only: [:dev, :test]},
       {:nimble_options, "~> 1.0"},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
